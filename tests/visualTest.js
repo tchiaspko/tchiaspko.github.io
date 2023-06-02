7f05ea89b637559deb904dd9a2a00ee9f5982c9d
Tony Chia <tchia@spokeo.com>
2023-06-02 10:17:44 -0700

    change to me

--- tests/visualTest.js
+++ tests/visualTest.js
@@ -31,7 +31,7 @@ var capabilities = {
     .build();
 
   try {
-    await driver.get('https://adarshts.github.io/percy-web-demo/', 1000);
+    await driver.get('https://tchiaspko.github.io/', 1000);
     await percySnapshot(driver, 'Homepage');
 
     await driver.wait(webdriver.until.elementLocated(By.xpath("//a[text()='Designers']"))).click();
